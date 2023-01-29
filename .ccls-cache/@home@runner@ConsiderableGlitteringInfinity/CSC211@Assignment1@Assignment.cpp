774eff0b88f8a03c6ecdac10c1da709736b20b7f
Pingbo Yang <86988793+PingboYang@users.noreply.github.com>
2023-01-29 19:39:14 +0000

    added assignment1

--- .ccls-cache/@home@runner@ConsiderableGlitteringInfinity/CSC211@Assignment1@Assignment.cpp
+++ .ccls-cache/@home@runner@ConsiderableGlitteringInfinity/CSC211@Assignment1@Assignment.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
+  int numOne;
+  int numTwo;
+  cout <<"Please enter number one.";
+  cin >> numOne;
+  cout << "Please enter number two";
+  cin >> numTwo;
+  if(numOne>numTwo){
+    cout << "Number one "<<numOne<<" is larger";
+  }else{
+    cout << "Number two "<<numTwo<<" is larger";
+  }
   return 0;
 }
--- CSC211/Assignment2/Assignment2.cpp
+++ CSC211/Assignment2/Assignment2.cpp
@@ -1,7 +1,11 @@
+
 #include <iostream>
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
+  int x;
+  cout << "Type a number: "; // Type a number and press enter
+  cin >> x; // Get user input from the keyboard
+  cout << "Your number is: " << x;
   return 0;
 }
